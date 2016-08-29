94c32b5f62aeff8de37a09dbd89e3c9e600cdf36
Hori873 <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2016-08-29 10:00:43 +0000

    Initial code and word lists for generating romanian settlement names

---
+++ UniversalNameGenerator/Views/MainMenu.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace UniversalNameGenerator.Views
+{
+    public class MainMenu : Menu
+    {
+        List<string> settlementBases, settlementSuffixes, settlementFilters;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UniversalNameGenerator.Views.MainMenu"/> class.
+        /// </summary>
+        public MainMenu()
+        {
+            Title = "Universal Name Generator [RO]";
+
+            AddCommand("settlements", "Generate 15 settlement names", GenerateSettlementNames);
+        }
+
+        /// <summary>
+        /// Generates 15 settlement names.
+        /// </summary>
+        void GenerateSettlementNames()
+        {
+            Random rnd = new Random();
+            int count;
+
+            LoadWords();
+
+            for (count = 0; count < 15; count++)
+            {
+                string nameBase = settlementBases[rnd.Next(0, settlementBases.Count)].ToLower();
+                string nameSuffix = settlementSuffixes[rnd.Next(0, settlementSuffixes.Count)].ToLower();
+                string name = nameBase + nameSuffix;
+
+                if (nameBase[nameBase.Length - 1] == nameSuffix[0])
+                    name = name.Remove(nameBase.Length - 1, 1);
+
+                if (!SettlementNameIsValid(name))
+                {
+                    count -= 1;
+                    continue;
+                }
+
+                // Capitalization
+                name = CultureInfo.GetCultureInfo("ro-RO").TextInfo.ToTitleCase(name);
+
+                Console.WriteLine(name);
+            }
+        }
+
+        void LoadWords()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(List<string>));
+
+            StreamReader settlementBasesSR = new StreamReader(
+                                                 Path.Combine("names", "romanian", "settlement_bases.xml"));
+            StreamReader settlementSuffixesSR = new StreamReader(
+                                                    Path.Combine("names", "romanian", "settlement_suffixes.xml"));
+            StreamReader settlementFiltersSR = new StreamReader(
+                                                   Path.Combine("names", "romanian", "settlement_filters.xml"));
+            
+
+            settlementBases = (List<string>)xs.Deserialize(settlementBasesSR);
+            settlementSuffixes = (List<string>)xs.Deserialize(settlementSuffixesSR);
+            settlementFilters = (List<string>)xs.Deserialize(settlementFiltersSR);
+
+            settlementBasesSR.Dispose();
+            settlementSuffixesSR.Dispose();
+            settlementFiltersSR.Dispose();
+        }
+
+        /// <summary>
+        /// Determines whether the settlement name is valid.
+        /// </summary>
+        /// <returns><c>true</c> if the settlement name is valid; otherwise, <c>false</c>.</returns>
+        /// <param name="name">Name.</param>
+        bool SettlementNameIsValid(string name)
+        {
+            foreach (string pattern in settlementFilters)
+                if (Regex.IsMatch(name, pattern))
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ UniversalNameGenerator/Views/Menu.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniversalNameGenerator.Views
+{
+    /// <summary>
+    /// Command-line Menu.
+    /// </summary>
+    public class Menu
+    {
+        string title, cmd;
+        bool isRunning;
+
+        Dictionary<string, string> commandTexts;
+        Dictionary<string, Action> commandActions;
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>The title.</value>
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UniversalNameGenerator.Views.Menu"/> class.
+        /// </summary>
+        public Menu()
+        {
+            commandTexts = new Dictionary<string, string>();
+            commandActions = new Dictionary<string, Action>();
+
+            AddCommand("exit", "Exit this menu", Exit);
+            AddCommand("print", "Print the command list", PrintCommandList);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UniversalNameGenerator.Views.Menu"/> class.
+        /// </summary>
+        /// <param name="title">Title.</param>
+        public Menu(string title)
+            : this()
+        {
+            this.title = title;
+        }
+
+        /// <summary>
+        /// Input the specified prompt.
+        /// </summary>
+        /// <param name="prompt">Prompt.</param>
+        public virtual string Input(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Inputs the permission.
+        /// </summary>
+        /// <returns><c>true</c>, if permission was input, <c>false</c> otherwise.</returns>
+        /// <param name="prompt">Prompt.</param>
+        public bool InputPermission(string prompt)
+        {
+            Console.Write(prompt);
+            Console.Write(" (y/N) ");
+
+            while (true)
+            {
+                ConsoleKeyInfo c = Console.ReadKey();
+
+                switch (c.Key)
+                {
+                    case ConsoleKey.Y:
+                        Console.WriteLine();
+                        return true;
+
+                    case ConsoleKey.N:
+                    case ConsoleKey.Enter:
+                        Console.WriteLine();
+                        return false;
+
+                    default:
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds the command.
+        /// </summary>
+        /// <param name="command">Command.</param>
+        /// <param name="text">Text.</param>
+        /// <param name="action">Action.</param>
+        public void AddCommand(string command, string text, Action action)
+        {
+            commandTexts.Add(command, text);
+            commandActions.Add(command, action);
+        }
+
+        /// <summary>
+        /// Runs this menu.
+        /// </summary>
+        public void Run()
+        {
+            Console.WriteLine("-===< {0} >===-" + Environment.NewLine, title);
+            isRunning = true;
+
+            PrintCommandList();
+
+            while (isRunning)
+            {
+                Console.WriteLine();
+
+                GetCommand();
+                HandleCommand();
+            }
+        }
+
+        /// <summary>
+        /// Prints the command list.
+        /// </summary>
+        void PrintCommandList()
+        {
+            int commandColumnWidth = commandTexts.Keys.Aggregate
+                ("", (max, cur) => max.Length > cur.Length ? max : cur).Length + 4;
+
+            foreach (KeyValuePair<string, string> entry in commandTexts)
+                Console.WriteLine("{0} {1}", entry.Key.PadRight(commandColumnWidth), entry.Value);
+        }
+
+        /// <summary>
+        /// Gets the command.
+        /// </summary>
+        /// <returns>The command.</returns>
+        string GetCommand()
+        {
+            Console.Write("> ");
+
+            cmd = Console.ReadLine();
+            return cmd;
+        }
+
+        /// <summary>
+        /// Handles the command.
+        /// </summary>
+        void HandleCommand()
+        {
+            foreach (string command in commandActions.Keys)
+                if (cmd == command)
+                {
+                    commandActions[cmd]();
+
+                    return;
+                }
+
+            Console.WriteLine("Invalid command");
+        }
+
+        /// <summary>
+        /// Exit this menu.
+        /// </summary>
+        void Exit()
+        {
+            isRunning = false;
+        }
+    }
+}
