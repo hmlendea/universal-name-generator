54cc8316d2a2f0b902d994a473c10d3eb3c6835a
Hori873 <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2016-08-30 21:40:00 +0000

    Added missing documentation comments

--- UniversalNameGenerator/Program.cs
+++ UniversalNameGenerator/Program.cs
@@ -2,8 +2,15 @@
 
 namespace UniversalNameGenerator
 {
+    /// <summary>
+    /// Main class.
+    /// </summary>
     class MainClass
     {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
         public static void Main(string[] args)
         {
             MainMenu mainMenu = new MainMenu();
--- UniversalNameGenerator/Views/MainMenu.cs
+++ UniversalNameGenerator/Views/MainMenu.cs
@@ -6,6 +6,9 @@ using System.Text.RegularExpressions;
 
 namespace UniversalNameGenerator.Views
 {
+    /// <summary>
+    /// Main menu.
+    /// </summary>
     public class MainMenu : Menu
     {
         List<string> settlementBases, settlementSuffixes, settlementFilters;
