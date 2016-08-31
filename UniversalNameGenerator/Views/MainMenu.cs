13ec204b16b34f057f92a7b24cd7194fa1e81a80
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2016-08-31 14:38:12 +0300

    Minor code refactoring and cleaning

--- UniversalNameGenerator/Views/MainMenu.cs
+++ UniversalNameGenerator/Views/MainMenu.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text.RegularExpressions;
-
-using UniversalNameGenerator.Models;
+﻿using UniversalNameGenerator.Models;
 using UniversalNameGenerator.Controllers;
 
 namespace UniversalNameGenerator.Views
