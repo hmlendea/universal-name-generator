ea92b2cee3da4387046b9fb37aa728909dfa8b5c
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-07-18 14:48:15 +0300

    Added the name generators from Narivia

---
+++ UniversalNameGenerator.BusinessLogic/Generators/Interfaces/INameGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalNameGenerator.BusinessLogic.Generators.Interfaces
+{
+    /// <summary>
+    /// Name Generator interface.
+    /// </summary>
+    public interface INameGenerator
+    {
+        /// <summary>
+        /// Gets or sets the minimum length of the name.
+        /// </summary>
+        /// <value>The minimum length of the name.</value>
+        int MinNameLength { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum length of the name.
+        /// </summary>
+        /// <value>The maximum length of the name.</value>
+        int MaxNameLength { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum processing time.
+        /// </summary>
+        /// <value>The maximum processing time in milliseconds.</value>
+        int MaxProcessingTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the excluded strings.
+        /// </summary>
+        /// <value>The excluded strings.</value>
+        List<string> ExcludedStrings { get; set; }
+
+        /// <summary>
+        /// Gets or sets the included strings.
+        /// </summary>
+        /// <value>The included strings.</value>
+        List<string> IncludedStrings { get; set; }
+
+        /// <summary>
+        /// Gets or sets the string that all generated names must start with.
+        /// </summary>
+        /// <value>The string start filter.</value>
+        string StartsWithFilter { get; set; }
+
+        /// <summary>
+        /// Gets or sets the string that all generated names must end with.
+        /// </summary>
+        /// <value>The string end filter.</value>
+        string EndsWithFilter { get; set; }
+
+        /// <summary>
+        /// Gets the used words.
+        /// </summary>
+        /// <value>The used words.</value>
+        List<string> UsedWords { get; }
+
+        /// <summary>
+        /// Generates a name.
+        /// </summary>
+        /// <returns>The name.</returns>
+        string GenerateName();
+
+        /// <summary>
+        /// Generates names.
+        /// </summary>
+        /// <returns>The names.</returns>
+        /// <param name="maximumCount">Maximum count.</param>
+        List<string> GenerateNames(int maximumCount);
+
+        /// <summary>
+        /// Reset the list of used names.
+        /// </summary>
+        void Reset();
+    }
+}
---
+++ UniversalNameGenerator.BusinessLogic/Generators/RandomMixerNameGenerator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Narivia.GameLogic.Generators
+{
+    /// <summary>
+    /// Random name generator that mixes words from different lists
+    /// </summary>
+    public class RandomMixerNameGenerator : AbstractNameGenerator
+    {
+        readonly int wordListsCount;
+
+        readonly List<string> wordList1;
+        readonly List<string> wordList2;
+        readonly List<string> wordList3;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomMixerNameGenerator"/> class.
+        /// </summary>
+        /// <param name="wordList1">Word list 1.</param>
+        /// <param name="wordList2">Word list 2.</param>
+        /// <param name="wordList3">Word list 3.</param>
+        public RandomMixerNameGenerator(List<string> wordList1, List<string> wordList2, List<string> wordList3)
+        {
+            wordListsCount = 3;
+
+            this.wordList1 = wordList1;
+            this.wordList2 = wordList2;
+            this.wordList3 = wordList3;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomMixerNameGenerator"/> class.
+        /// </summary>
+        /// <param name="wordList1">Word list1.</param>
+        /// <param name="wordList2">Word list2.</param>
+        public RandomMixerNameGenerator(List<string> wordList1, List<string> wordList2)
+        {
+            wordListsCount = 2;
+
+            this.wordList1 = wordList1;
+            this.wordList2 = wordList2;
+        }
+
+        /// <summary>
+        /// Generates a name.
+        /// </summary>
+        /// <returns>The name.</returns>
+        public override string GenerateName()
+        {
+            string name = string.Empty;
+
+            DateTime startTime = DateTime.Now;
+
+            while (DateTime.Now < startTime.AddMilliseconds(MaxProcessingTime) &&
+                   !IsNameValid(name))
+            {
+                name = GetRandomName();
+            }
+
+            return name;
+        }
+
+        string GetRandomName()
+        {
+            string name = string.Empty;
+
+            switch (wordListsCount)
+            {
+                case 2:
+                    name = wordList1[random.Next(wordList1.Count)] +
+                           wordList2[random.Next(wordList2.Count)];
+                    break;
+
+                case 3:
+                    name = wordList1[random.Next(wordList1.Count)] +
+                           wordList2[random.Next(wordList2.Count)] +
+                           wordList3[random.Next(wordList3.Count)];
+                    break;
+            }
+
+            return name;
+        }
+    }
+}
