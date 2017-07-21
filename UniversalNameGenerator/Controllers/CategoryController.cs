844d180d8cdfd6367b8cc0129311fa5f64ba9bf7
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2017-07-21 16:42:34 +0300

    Refactoring: Moved all logic in BL and improved it

--- UniversalNameGenerator/Controllers/CategoryController.cs
+++ UniversalNameGenerator/Controllers/CategoryController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Text.RegularExpressions;
-using System.Globalization;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 
-using UniversalNameGenerator.BusinessLogic.Generators;
-using UniversalNameGenerator.BusinessLogic.Generators.Interfaces;
+using UniversalNameGenerator.BusinessLogic;
+using UniversalNameGenerator.BusinessLogic.Interfaces;
 using UniversalNameGenerator.Models;
 using UniversalNameGenerator.DataAccess.Repositories;
 
@@ -26,8 +23,10 @@ namespace UniversalNameGenerator.Controllers
         /// <value>The language.</value>
         public Language Language { get; private set; }
 
+        IGeneratorManager generator;
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="UniversalNameGenerator.Controllers.CategoryController"/> class.
+        /// Initializes a new instance of the <see cref="CategoryController"/> class.
         /// </summary>
         /// <param name="language">Language.</param>
         public CategoryController(Language language)
@@ -36,6 +35,8 @@ namespace UniversalNameGenerator.Controllers
 
             repositoryFilePath = Path.Combine(MainClass.ApplicationDirectory, "Languages", Language.Id, "Categories.xml");
             rnd = new Random();
+
+            generator = new GeneratorManager();
         }
 
         /// <summary>
@@ -108,128 +109,9 @@ namespace UniversalNameGenerator.Controllers
             repository.Remove(id);
         }
 
-        /// <summary>
-        /// Generates a name.
-        /// </summary>
-        /// <param name="id">Category identifier.</param>
-        public string GenerateName(string id)
+        public IEnumerable<string> GenerateNames(string categoryId, int amount)
         {
-            RepositoryXml<Category> repository = new RepositoryXml<Category>(repositoryFilePath);
-            Category category = repository.Get(id);
-
-            List<string> filters;
-            Dictionary<string, List<string>> wordlists = new Dictionary<string, List<string>>();
-
-            INameGenerator generator;
-
-            string name = string.Empty;
-            
-            // Load the wordlists
-            foreach (string wordlistId in category.Wordlists)
-            {
-                List<string> wordlist = new List<string>(File.ReadAllLines(
-                                                Path.Combine(MainClass.ApplicationDirectory, "Languages",
-                                                    Language.Id, wordlistId + ".txt")));
-
-                wordlists.Add(wordlistId, wordlist);
-            }
-
-            // Load the filters
-            if (!string.IsNullOrWhiteSpace(category.Filterlist))
-            {
-                filters = new List<string>(File.ReadAllLines(
-                        Path.Combine(MainClass.ApplicationDirectory, "Languages",
-                            Language.Id, category.Filterlist + ".txt")));
-            }
-            else
-            {
-                filters = new List<string>();
-            }
-
-            while (!NameIsValid(name, filters))
-            {
-                name = category.GenerationSchema;
-
-                while (name.Contains("{") || name.Contains("}"))
-                {
-                    int pos = name.IndexOf('{') + 1;
-                    string com = name.Substring(pos, name.IndexOf('}') - pos);
-                    string value = "";
-
-                    string[] split = com.Split(',');
-
-                    switch (split[0])
-                    {
-                        case "random":
-                            value = RandomString(split[1].Split('|').ToList(), int.Parse(split[2]), int.Parse(split[3]));
-
-                            break;
-
-                        case "randomiser":
-                            List<string> wordlistKeys = split[2].Split('|').ToList();
-
-                            generator = new RandomMixerNameGenerator(split[1], wordlistKeys.Select((k) => wordlists[k]).ToList());
-                            generator.ExcludedStrings = filters;
-
-                            value = generator.GenerateName();
-                            break;
-
-                        case "markov":
-                            generator = new MarkovNameGenerator(wordlists[split[1]]);
-                            generator.ExcludedStrings = filters;
-
-                            value = generator.GenerateName();
-                            break;
-                    }
-
-                    name = name.Replace("{" + com + "}", value);
-                }
-            }
-
-            // Capitalization
-            name = CultureInfo.GetCultureInfo("ro-RO").TextInfo.ToTitleCase(name);
-
-            return name;
-        }
-
-        /// <summary>
-        /// Determines whether the name is valid, based on a list of filters.
-        /// </summary>
-        /// <returns><c>true</c> if the name is valid; otherwise, <c>false</c>.</returns>
-        /// <param name="name">Name.</param>
-        /// <param name="filters">Filters.</param>
-        bool NameIsValid(string name, List<string> filters)
-        {
-            if (string.IsNullOrWhiteSpace(name))
-                return false;
-
-            foreach (string pattern in filters)
-                if (Regex.IsMatch(name, pattern))
-                    return false;
-
-            return true;
-        }
-
-        /// <summary>
-        /// Generates a random string.
-        /// </summary>
-        /// <returns>The string.</returns>
-        /// <param name="charlist">Charlist.</param>
-        /// <param name="minLength">Minimum length.</param>
-        /// <param name="maxLength">Maximum length.</param>
-        string RandomString(List<string> choices, int minLength, int maxLength)
-        {
-            Random rnd = new Random();
-            string str = "";
-            int targetLength = rnd.Next(minLength, maxLength + 1);
-
-            while (str.Length != targetLength)
-            {
-                int i = rnd.Next(0, choices.Count);
-                str += choices[i];
-            }
-
-            return str;
+            return generator.GenerateNames(Language.Id, categoryId, amount);
         }
     }
 }
