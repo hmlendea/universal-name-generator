548f79eb80accbded6c7e91f0e42fce13a88941f
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2017-08-16 17:56:21 +0300

    Reimplemented the whole DataAccess

---
+++ UniversalNameGenerator.DataAccess/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UniversalNameGenerator.DataAccess.Exceptions
+{
+    /// <summary>
+    /// Repository exception.
+    /// </summary>
+    public class EntityNotFoundException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DuplicateEntityException"/> class.
+        /// </summary>
+        /// <param name="entityId">Entity identifier.</param>
+        /// <param name="entityType">Entity type.</param>
+        public EntityNotFoundException(string entityId, string entityType)
+            : base($"The {entityId} {entityType} entity can not be found.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityNotFoundException"/> class.
+        /// </summary>
+        /// <param name="entityId">Entity identifier.</param>
+        /// <param name="entityType">Entity type.</param>
+        /// <param name="innerException">Inner exception.</param>
+        public EntityNotFoundException(string entityId, string entityType, Exception innerException)
+            : base($"The {entityId} {entityType} entity can not be found.", innerException)
+        {
+        }
+    }
+}
