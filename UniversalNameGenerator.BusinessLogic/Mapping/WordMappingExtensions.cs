d31234de5a2a2ea5c676c71cc8ea45491879eecc
Horatiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2018-02-02 12:52:54 +0200

    Created the Word entity

---
+++ UniversalNameGenerator.BusinessLogic/Mapping/WordMappingExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using UniversalNameGenerator.DataAccess.DataObjects;
+using UniversalNameGenerator.Models;
+
+namespace UniversalNameGenerator.BusinessLogic.Mapping
+{
+    /// <summary>
+    /// Generation schema mapping extensions for converting between entities and domain models.
+    /// </summary>
+    static class WordMappingExtensions
+    {
+        /// <summary>
+        /// Converts the entity into a domain model.
+        /// </summary>
+        /// <returns>The domain model.</returns>
+        /// <param name="wordEntity">Word entity.</param>
+        internal static Word ToDomainModel(this WordEntity wordEntity)
+        {
+            Word word = new Word
+            {
+                Id = wordEntity.Id,
+                Values = wordEntity.Values
+            };
+
+            return word;
+        }
+
+        /// <summary>
+        /// Converts the domain model into an entity.
+        /// </summary>
+        /// <returns>The entity.</returns>
+        /// <param name="word">Word.</param>
+        internal static WordEntity ToEntity(this Word word)
+        {
+            WordEntity wordEntity = new WordEntity
+            {
+                Id = word.Id,
+                Values = word.Values
+            };
+
+            return wordEntity;
+        }
+
+        /// <summary>
+        /// Converts the entities into domain models.
+        /// </summary>
+        /// <returns>The domain models.</returns>
+        /// <param name="wordEntities">Word entities.</param>
+        internal static IEnumerable<Word> ToDomainModels(this IEnumerable<WordEntity> wordEntities)
+        {
+            IEnumerable<Word> words = wordEntities.Select(wordEntity => wordEntity.ToDomainModel());
+
+            return words;
+        }
+
+        /// <summary>
+        /// Converts the domain models into entities.
+        /// </summary>
+        /// <returns>The entities.</returns>
+        /// <param name="words">Words.</param>
+        internal static IEnumerable<WordEntity> ToEntities(this IEnumerable<Word> words)
+        {
+            IEnumerable<WordEntity> wordEntities = words.Select(word => word.ToEntity());
+
+            return wordEntities;
+        }
+    }
+}
---
+++ UniversalNameGenerator.Models/Word.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalNameGenerator.Models
+{
+    public sealed class Word
+    {
+        public string Id { get; set; }
+
+        public IEnumerable<string> Values { get; set; }
+    }
+}
