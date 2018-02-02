bc422efb1e12058e711890c3a7c1ee4726fa4940
Horatiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2018-02-02 13:16:18 +0200

    Word repository

---
+++ UniversalNameGenerator.DataAccess/Repositories/Interfaces/IWordRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using UniversalNameGenerator.DataAccess.DataObjects;
+
+namespace UniversalNameGenerator.DataAccess.Repositories.Interfaces
+{
+    /// <summary>
+    /// Word repository interface.
+    /// </summary>
+    public interface IWordRepository
+    {
+        /// <summary>
+        /// Gets all the words.
+        /// </summary>
+        /// <returns>The words</returns>
+        IEnumerable<WordEntity> GetAll();
+    }
+}
---
+++ UniversalNameGenerator.DataAccess/Repositories/WordRepository.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using UniversalNameGenerator.DataAccess.DataObjects;
+using UniversalNameGenerator.DataAccess.Repositories.Interfaces;
+
+namespace UniversalNameGenerator.DataAccess.Repositories
+{
+    /// <summary>
+    /// Word repository implementation.
+    /// </summary>
+    public class WordRepository : IWordRepository
+    {
+        readonly Dictionary<string, WordEntity> words;
+        readonly string fileName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WordRepository"/> class.
+        /// </summary>
+        /// <param name="fileName">File name.</param>
+        public WordRepository(string fileName)
+        {
+            words = new Dictionary<string, WordEntity>();
+
+            this.fileName = fileName;
+        }
+
+        /// <summary>
+        /// Gets all the generation words.
+        /// </summary>
+        /// <returns>The generation words</returns>
+        public IEnumerable<WordEntity> GetAll()
+        {
+            return words.Values;
+        }
+
+        void LoadContent()
+        {
+            using (StreamReader reader = File.OpenText(fileName))
+            {
+                string line = string.Empty;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int separatorIndex = line.IndexOf('_');
+
+                    string id = string.Empty;
+                    string value = string.Empty;
+
+                    if (separatorIndex > 0)
+                    {
+                        value = line.Substring(0, separatorIndex);
+                        id = line.Substring(separatorIndex + 1);
+                    }
+                    else
+                    {
+                        id = line;
+                        value = line;
+                    }
+
+                    if (words.ContainsKey(id))
+                    {
+                        words[id].Values.Add(value);
+                    }
+                    else
+                    {
+                        WordEntity word = new WordEntity
+                        {
+                            Id = id,
+                            Values = new List<string> { value }
+                        };
+
+                        words.Add(word.Id, word);
+                    }
+                }
+            }
+        }
+    }
+}
