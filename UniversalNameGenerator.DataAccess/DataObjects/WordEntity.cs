c4f953e69ce7b2e3ee8cbc2a97ccf81b69f64abb
Horatiu Mlendea <mlendea.horatiu@gmail.com>
2018-02-02 12:52:54 +0200

    Created the Word entity

---
+++ UniversalNameGenerator.DataAccess/DataObjects/WordEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace UniversalNameGenerator.DataAccess.DataObjects
+{
+    public sealed class WordEntity
+    {
+        public string Id { get; set; }
+
+        public IEnumerable<string> Values { get; set; }
+
+        public WordEntity()
+        {
+            Values = new List<string>();
+        }
+    }
+}
