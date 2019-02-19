0d43442035893fdc1a0ebe6962ad7acfc7893a00
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2019-02-19 15:11:21 +0200

    Use the NuciCLI package

--- UniversalNameGenerator/Menus/MainMenu.cs
+++ UniversalNameGenerator/Menus/MainMenu.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using NuciCLI.Menus;
+
 using UniversalNameGenerator.BusinessLogic.GenerationManagers;
 using UniversalNameGenerator.BusinessLogic.GenerationManagers.Interfaces;
 using UniversalNameGenerator.Models;
